f16007e29a305ddd1b19f90621201b4c5ddd538e
Nektus <danny.nguyen2@ucalgary.ca>
2025-10-05 06:55:02 -0600

    little fix to userschema

--- backend/models/User.js
+++ backend/models/User.js
@@ -45,8 +45,7 @@ const UserSchema = new mongoose.Schema({
     levels: { type: Number, default: 3 },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
-  }]
-  },
+  }],
   googleId: {
     type: String,
     default: null
